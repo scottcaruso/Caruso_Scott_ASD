62fb6eea9a7166844ec7db4188c4142feb3b9666
Scott Caruso <scottcaruso@fullsail.edu>
2012-08-23 01:18:47 -0400

    Backing up views.

---
+++ views/type-curse/map.js
@@ -0,0 +1,15 @@
+function(doc) {
+	if (doc.type === "Enchantment-Curse") {
+	    emit(doc.name, {
+	    	"id" : doc._id,
+	    	"rev" : doc._rev,
+	    	"name" : doc.name,
+	    	"colors" : doc.colors,
+	    	"mana" : doc.mana,
+	    	"notes" : doc.notes,
+	    	"number" : doc.number,
+	    	"type" : doc.type,
+	    	"usage" : doc.usage
+		    });
+		}
+};
